3590ffe479ff29a32186d039f6583c1149c24b1e
UKA SHOU <719469507@qq.com>
2026-04-14 13:32:48 +0800

    Create my0401.java

---
+++ my0401.java
@@ -0,0 +1,13 @@
+/**
+ * Welcome类（文档注释）
+ * @version 1.0
+ */
+public class my0401 { // 单行注释
+    public static void main(String[] args /* 行内注释 */) {
+        System.out.println("Welcome to CJLU!");
+    }
+    /*
+     * 多行注释！
+     * 多行注释！
+     */
+}
