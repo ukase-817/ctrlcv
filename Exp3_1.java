6ec82398771e4150d3a8860a575ea01e9c34b4de
UKA SHOU <719469507@qq.com>
2026-04-19 12:39:54 +0800

    Update Exp3_1.java

--- Exp3_1.java
+++ Exp3_1.java
@@ -1,6 +1,5 @@
 import java.util.Date;
 import java.time.LocalDate;
-
 public class Exp3_1 {
     public static void main(String[] args) {
         Date today = new Date();
@@ -8,7 +7,5 @@ public class Exp3_1 {
         LocalDate localDate = LocalDate.now();
         System.out.println(localDate.getMonthValue());
         System.out.println(localDate.getYear());
-
-    }
-    
+    }  
 }
